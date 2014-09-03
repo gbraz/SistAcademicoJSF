0179dac7ce0a9f65f05eb7170185ed8e2e9b70c1
Matheus Mayron <matheusmayron@gmail.com>
2014-09-03 15:39:36 -0300

    poucas modificações

--- src/br/ufc/mbean/CadastrarAlunoBean.java
+++ src/br/ufc/mbean/CadastrarAlunoBean.java
@@ -1,16 +1,38 @@
 package br.ufc.mbean;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
+import br.ufc.control.OperacoesEmAlunos;
 import br.ufc.model.Aluno;
+import br.ufc.model.Sexo;
 
-@ManagedBean(name="cadastrarAluno")
+@ManagedBean(name = "cadastrarAluno")
+@SessionScoped
 public class CadastrarAlunoBean {
 
 	private Aluno aluno = new Aluno();
-	
-	public void cadastrar() {
-		System.out.println("Teste");
+	private Integer contador = 0;
+		
+	public CadastrarAlunoBean() {
+		aluno.setNome("Matheus");
+	}
+
+	public String cadastrar() {
+		Calendar nascimento = Calendar.getInstance();
+		OperacoesEmAlunos.inserirAluno(aluno.getMatricula(), aluno.getNome(), nascimento , Sexo.HOMEM, aluno.getCpf());
+		System.out.println("Teste: " + aluno.getNome() +"\n CPF: "+aluno.getCpf()+"\nContador: "
+				+ contador++);
+		System.out.println(OperacoesEmAlunos.getAluno(aluno.getMatricula()).toString());
+		return "confirma";
+	}
+
+	public HashMap<Integer, Aluno> getAlunos(){
+		return OperacoesEmAlunos.getAlunos();
 	}
 	
 	public Aluno getAluno() {
@@ -20,6 +42,5 @@ public class CadastrarAlunoBean {
 	public void setAluno(Aluno aluno) {
 		this.aluno = aluno;
 	}
-	
-	
+
 }
