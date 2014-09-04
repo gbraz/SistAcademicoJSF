c5d22704763167642f58988b7bb4d6637eccf9fb
abevieiramota <abevieiamota@gmail.com>
2014-09-04 07:37:10 -0300

    adicionado atributo nascimento à listagem dos alunos - ver que dessa forma, não é impressa a data, mas uma representação do objeto

--- src/br/ufc/model/Sexo.java
+++ src/br/ufc/model/Sexo.java
@@ -2,13 +2,13 @@ package br.ufc.model;
 
 public enum Sexo {
 	HOMEM("Homem"), MULHER("Mulher"), OUTRO("Outro");
-	
+
 	private String _asString;
-	
+
 	Sexo(String asString) {
 		_asString = asString;
 	}
-	
+
 	public String toString() {
 		return _asString;
 	}
