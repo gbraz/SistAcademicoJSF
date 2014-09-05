175cd2af44f0c87017397ffbac2e452aa9bce9d1
Matheus Mayron <matheusmayron@gmail.com>
2014-09-05 15:13:58 -0300

    Teste adicionado, outputText substituido.

--- test/br/ufc/model/AlunoTeste.java
+++ test/br/ufc/model/AlunoTeste.java
@@ -1,5 +1,59 @@
 package br.ufc.model;
 
-public class AlunoTeste {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ufc.control.OperacoesEmAlunos;
+
+public class AlunoTeste {
+	@Test
+	public void testCriar() {
+		// Dados do aluno
+		int matricula = 2;
+		String nome = "Geraldo";
+		Sexo sexo = Sexo.OUTRO;
+		String cpf = "666";
+		Calendar data = new GregorianCalendar();
+		data.set(2015, 10, 12);
+		OperacoesEmAlunos.inserirAluno(matricula, nome, data, sexo, cpf);
+		Aluno aluno = OperacoesEmAlunos.getAluno(2);
+		// testa os atributos
+		Assert.assertEquals(cpf, aluno.getCpf());
+		Assert.assertEquals(matricula, aluno.getMatricula());
+		Assert.assertEquals(nome, aluno.getNome());
+		Assert.assertEquals(sexo, aluno.getSexo());
+		Assert.assertEquals(data, aluno.getNascimento());
+	}
+
+	@Test
+	public void testRemover() {
+		// Dados do aluno
+		int matricula = 2;
+		String nome = "Geraldo";
+		Sexo sexo = Sexo.OUTRO;
+		String cpf = "666";
+		Calendar data = new GregorianCalendar();
+		data.set(2015, 10, 12);
+		OperacoesEmAlunos.inserirAluno(matricula, nome, data, sexo, cpf);
+		// remove o aluno
+		OperacoesEmAlunos.removerAluno(matricula);
+		Assert.assertTrue(OperacoesEmAlunos.getAlunos().isEmpty());
+	}
+
+	@Test
+	public void testEditar() {
+		Aluno aluno;
+		Calendar data = new GregorianCalendar();
+		data.set(2015, 10, 12);
+		OperacoesEmAlunos.inserirAluno(6, "Zordon", data, Sexo.OUTRO,"00000-00");
+		aluno = new Aluno(6, "Alpha", data, Sexo.MULHER, "111111-00");
+		Assert.assertTrue(OperacoesEmAlunos.editarAluno(6, "Alpha", data,Sexo.MULHER, "111111-00"));
+		Assert.assertTrue(OperacoesEmAlunos.getAluno(6).equals(aluno));
+	}
 }
